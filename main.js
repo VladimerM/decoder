e54edd80461e945dbe4eab950bb8e54c3e9b1866
Vladimer <mazmanyanvladimer@gmail.com>
2023-11-19 21:58:28 +0400

    commit

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const input = document.getElementById("input");
-const dummyInput = document.getElementById("dummy-input");
 const output = document.getElementById("output");
 const clearButton = document.getElementById("clear");
+const copyButton = document.getElementById("copy");
 const charmaps = {
   utf: "აბგდევზთიკლმნოპჟრსტუფქღყშჩცძწჭხჯჰ",
   lat: "abgdevzTiklmnopJrstufqRySCcZwWxjh",
@@ -10,21 +10,20 @@ const convertEncoding = function (subject, fromChars, toChars) {
   var re = "";
   for (i = 0; i < fromChars.length; i++) {
     re = new RegExp(fromChars[i], "gm");
-    if (re.test("")) {
-      subject = subject.replace(re, "");
-      continue;
-    }
     subject = subject.replace(re, toChars[i]);
   }
   return subject;
 };
 
 input.addEventListener("input", () => {
-  dummyInput.value = convertEncoding(input.value, charmaps.lat, charmaps.utf);
-  output.value = dummyInput.value;
+  output.textContent = convertEncoding(input.value, charmaps.lat, charmaps.utf);
 });
 
 clearButton.addEventListener("click", () => {
   output.value = "";
   input.value = "";
+});
+
+copyButton.addEventListener("click", () => {
+  navigator.clipboard.writeText(output.value);
 });
